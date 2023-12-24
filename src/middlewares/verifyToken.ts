047c6ed1bc0638d3f665fd2025d1f195403531af
vocongbinh <binh93phudong@gmail.com>
2023-12-24 09:32:09 +0700

    h

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -6,14 +6,12 @@ const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   console.log(authHeader);
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    const binh = jwt.verify(token);
-    return res.json(binh)
-    // jwt.verify(token, async (err: any, user: any) => {
-    //   if (err) return res.status(403).json(err);
-    //   req.body.user = user;
-    //   console.log(user);
-    //   next();
-    // });
+    jwt.verify(token, process.env.SECRET,async (err: any, user: any) => {
+      if (err) return res.status(403).json(err);
+      req.body.user = user;
+      console.log(user);
+      next();
+    });
   } else {
     return res.status(401).json("You are not authenticated");
   }
